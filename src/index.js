cf4ffcfc372685ed9cf859bbc7083d6606ea370e
Sergey Emelyanov <post.es2010@yandex.ru>
2023-02-09 21:35:23 +0300

    slider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import 'normalize.css'
-import './styles/main.scss'
 import './index.html'
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import './styles/main.scss'
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+var swiper = new Swiper(".offerSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  // loop: true,
+  loopFillGroupWithBank: true,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  modules: [Navigation, Pagination],
+});

5e8c01c35dcb5c90fd4793f4bf04b35104767f83
Sergey notebook <post.es2010@yandex.ru>
2023-03-23 13:47:38 +0300

    add trends

--- src/index.js
+++ src/index.js
@@ -24,3 +24,20 @@ var swiper = new Swiper(".offerSwiper", {
   },
   modules: [Navigation, Pagination],
 });
+
+var swiper = new Swiper(".trendsSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBank: true,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  modules: [Navigation, Pagination],
+});
